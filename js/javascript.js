87a9eb396422e34867fec44966f5f2744edeca54
maribel <maribel.diaz.j@gmail.com>
2017-04-24 15:52:00 -0300

    mostrar grados celcius a farenheit

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+function converCelsius(grados) {
+	var cel = document.getElementById("fahrenheit").value;
+	var operacion = (cel*(9/5))+32;
+var resultado = document.getElementById("Calcular").value = operacion;
+
+}
